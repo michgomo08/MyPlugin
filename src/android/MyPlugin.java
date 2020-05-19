825f66581a206e9922d104330d1fac0cad633006
Michael <michgomo08.2@gmail.com>
2020-05-19 09:40:54 -0600

    Commit 20

--- src/android/MyPlugin.java
+++ src/android/MyPlugin.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Build;
+import android.widget.Toast;// ToToast
+import android.os.Bundle;
    
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -22,21 +24,15 @@ import org.json.JSONObject;
  */
 public class MyPlugin extends CordovaPlugin {
     
-    public final int MY_OP = 8;
-    private CallbackContext callback = null;
-    private String title ="";
-
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-    }
+    private String title =""; 
+    private CallbackContext PUBLIC_CALLBACKS = null;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 		
 		try {
-            
-            callback = callbackContext;
+
+            PUBLIC_CALLBACKS = callbackContext;
             
             if (action.equals("testMichael")) {
                 String message = args.getString(0) + " (desde MyPlugin) --- testMichael";
@@ -46,19 +42,19 @@ public class MyPlugin extends CordovaPlugin {
             else if(action.equals("new_activity")) {
                 title= args.getString(0) + " (desde MyPlugin) --- new_activity";
 
-                cordova.setActivityResultCallback (this);
-        
-               /* Intent intent = new Intent();
-                intent.setClassName("com.mgm","com.mgm.NewActivity");*/
+                // The intent expects as first parameter the given name for the activity in your plugin.xml
+            Intent intent = new Intent("com.mgm.NewActivity");
+            // Send some info to the activity to retrieve it later
+            intent.putExtra("title", title);
+            // Now, cordova will expect for a result using startActivityForResult and will be handle by the onActivityResult.
+            cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
 
-                Intent intent = new Intent(this.cordova.getActivity(), NewActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                intent.putExtra("title", title);
-        
-                cordova.startActivityForResult (this, intent, MY_OP);
-                return true;
             }
-            return false;
+            // Send no result, to execute the callbacks later
+            PluginResult pluginResult = new  PluginResult(PluginResult.Status.NO_RESULT);
+            pluginResult.setKeepCallback(true); // Keep callback
+
+            return true;
         } catch (JSONException e) {
 			callbackContext.error("Reminder exception occured: "+e.toString());
 			return false;
@@ -77,24 +73,36 @@ public class MyPlugin extends CordovaPlugin {
             }
     }
 
+
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) 
-    {
-        if( requestCode == MY_OP )
-        {
-            if( resultCode == Activity.RESULT_OK && data.hasExtra("return_val") )
-            {
-                PluginResult result = new PluginResult(PluginResult.Status.OK, data.getStringExtra("return_val"));
-                result.setKeepCallback(true);
-                callback.sendPluginResult(result);
-            }
-            else
-            {
-                PluginResult result = new PluginResult(PluginResult.Status.ERROR, "no params returned successfully" );
-                result.setKeepCallback(true);
-                callback.sendPluginResult(result);
-            }
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if(resultCode == cordova.getActivity().RESULT_OK){
+            Bundle extras = data.getExtras();// Get data sent by the Intent
+            String information = extras.getString("data"); // data parameter will be send from the other activity.
+            tolog(information); // Shows a toast with the sent information in the other class
+            PluginResult resultado = new PluginResult(PluginResult.Status.OK, "this value will be sent to cordova");
+            resultado.setKeepCallback(true);
+            PUBLIC_CALLBACKS.sendPluginResult(resultado);
+            return;
+        }else if(resultCode == cordova.getActivity().RESULT_CANCELED){
+            PluginResult resultado = new PluginResult(PluginResult.Status.OK, "canceled action, process this in javascript");
+            resultado.setKeepCallback(true);
+            PUBLIC_CALLBACKS.sendPluginResult(resultado);
+            return;
         }
+        // Handle other results if exists.
+        super.onActivityResult(requestCode, resultCode, data);
     }
+    
+    // A function to show a toast with some data, just demo
+    public void tolog(String toLog){
+        Context context = cordova.getActivity();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, toLog, duration);
+        toast.show();
+    }
+}
+    
 
 }
--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -1,14 +1,41 @@
 package com.mgm;
 
 import android.app.Activity;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Intent;
 import android.os.Bundle;
    
 public class NewActivity extends Activity {
-   
+    private boolean firstTime = true;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        String package_name = getApplication().getPackageName();
-        setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
+    public void onStart() {
+        super.onStart();
+        // Write your code inside this condition
+        // Here should start the process that expects the onActivityResult
+        if(firstTime == true){
+            // Do something at first initialization
+            // And retrieve the parameters that we sent before in the Main file of the plugin
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+               String tittle = extras.getString("tittle");
+               
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        firstTime = false;// There's a result, allow to exit the activity !
+        
+        // Do something with the result of the Intent data
+ 
+        // Send parameters to retrieve in cordova.
+        Intent intent = new Intent();
+        intent.putExtra("data", "This is the sent information from the 2 activity :) ");
+        setResult(Activity.RESULT_OK, intent);
+        finish();// Exit of this activity !
     }
 }
